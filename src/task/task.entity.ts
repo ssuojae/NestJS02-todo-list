b0ae09045869e3f1df176743ac7762ea7a13652e
suojae <suojae.developer@gmail.com>
2024-09-17 14:24:14 +0900

    feat: task entity 추가

---
+++ src/task/task.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: 'pending' })
+  status: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
