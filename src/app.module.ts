974f6266f3b310d5326d4b528e8cf65a616711d5
suojae <suojae.developer@gmail.com>
2024-09-17 14:20:08 +0900

    feat: typeorm module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'mydb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], //.entity.ts 파일 자동 연동
+      synchronize: true, // 프로덕션 환경에서는 false 로 해야함 - 자동연동x
+    }),
+  ],
 })
 export class AppModule {}
